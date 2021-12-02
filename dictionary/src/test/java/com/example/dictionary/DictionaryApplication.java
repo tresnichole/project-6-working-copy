b9772fd51fcfc70a2df1c7bac04e067b5d416aee
tressa <tressanortiz@gmail.com>
2021-12-01 21:52:37 -0700

    another 1

---
+++ dictionary/src/test/java/com/example/dictionary/DictionaryApplication.java
@@ -0,0 +1,10 @@
+package com.example.dictionary;
+
+import org.springframework.boot.SpringApplication;
+
+public class DictionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryApplication.class, args);
+    }
+}
