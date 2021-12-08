9720a01660cb0ff0bb175502ca14f5015868eee4
Spyro1001 <30807727+Spyro1001@users.noreply.github.com>
2021-12-07 19:41:53 -0700

    12/7/21 adding changes for project 6

--- aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
+++ aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AggregatorController {
 
@@ -19,5 +21,11 @@ public class AggregatorController {
     public Entry getDefinitionFor(@PathVariable String word) {
 
         return service.getDefinitionFor(word);
+    }
+
+    @GetMapping("/getWordsStartingWith/{chars}")
+    public List<Entry> getWordsStartingWith(@PathVariable String chars) {
+
+        return service.getWordsStartingWith(chars);
     }
 }
